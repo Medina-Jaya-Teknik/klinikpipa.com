382692a07c476afaa75d30d550010d4c346bc687
aden <aden.pribadi@gmail.com>
2026-08-20 09:19:43 +0700

    fix(seo): add rewrite for sitemaps.xml and sitemap_index.xml to point to sitemap.xml

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/sitemaps.xml",
+        destination: "/sitemap.xml",
+      },
+      {
+        source: "/sitemap_index.xml",
+        destination: "/sitemap.xml",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
